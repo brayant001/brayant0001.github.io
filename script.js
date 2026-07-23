0b5e99df9364817bfb000238845202e3f7b96b5a
brayant001 <brayted14@gmail.com>
2026-07-23 08:24:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   const targets = {
-    downloadCvBtn: 'https://drive.google.com/file/d/1TIO03R3LbEBuT7RCandO20BUaJWXqp9u/view?usp=sharing',
+    downloadCvBtn: 'https://drive.google.com/file/d/1bYADs6s2ZN88R5zHn1Awn70xY0LCgnlX/view?usp=sharing',
     downloadartBtn: 'https://drive.google.com/file/d/12flPZh-0wRGFLCqNi4QDK7eZlcF_Vy_H/view?usp=sharing',
     downloadart2Btn: 'https://doi.org/10.70747/cr.v5i1.977',
     downloadCGDABtn: 'https://drive.google.com/file/d/1jE_dRaHqYMw575IVxFHFnR2EmDQMdIBs/view?usp=sharing',
